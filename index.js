42732a047398f98392929b0ebd407863a0d12a2c
Rugsoft <futuroelectronico@gmail.com>
2026-05-07 13:32:41 +0200

    añadida funcion de generar password aleatorio

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
-const btnEl = document.querySelector(".btn")
+const btnEl = document.querySelector(".btn");
 
 btnEl.addEventListener("click", ()=>{
 
-    createPassword()
-})
+    createPassword();
+});
 
+function createPassword(){
+
+    const chars = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    const passwordLength = 14; 
+    let password = "";
+
+    for (let index = 0; index < passwordLength; index++) {
+       const randomNum = Math.floor(Math.random() * chars.length);
+       password += chars.substring(randomNum, randomNum + 1);
+       console.log(randomNum, password);
+    }
+}
